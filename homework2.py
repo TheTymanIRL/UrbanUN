bc8ca9e478ca09bbcdb22170cb56647a8ec1262d
TheTymanIRL <artiym@inbox.ru>
2024-07-16 20:37:33 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+lessons_done = 12
+hours = 1.5
+name_of_course = 'Python'
+time_per_lesson = hours / lessons_done
+print('Курс: ' + name_of_course + ', всего задач: ' + str(lessons_done) + ', затрачено часов: ' + str(hours) + ', среднее время выполнения: ' + str(time_per_lesson) + ' часа.')
