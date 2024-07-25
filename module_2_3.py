1499d5dc6b8c226082db25a094d990a2148e6c69
TheTymanIRL <artiym@inbox.ru>
2024-07-26 03:34:52 +0500

    Исправление ошибок

--- module_2_3.py
+++ module_2_3.py
@@ -1,12 +1,11 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 i = 0
-print(len(my_list)-1)
-print(my_list[0])
 while True:
     if i <= len(my_list)-1:
-        if my_list[i] >= 0:
+        if my_list[i] > 0:
             print(my_list[i])
             i += 1
-            continue
+        elif my_list[i] == 0:
+            i += 1
         else:
             break
