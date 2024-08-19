6dbb5819ed1d2130f60f34fa40740b9a25848d57
TheTymanIRL <artiym@inbox.ru>
2024-08-19 22:59:49 +0500

    Update module_4_2.py

--- module_4_2.py
+++ module_4_2.py
@@ -3,8 +3,9 @@
 def test_function():
     def inner_function():
         print('Я в области видимости функции test_function')
-    print(inner_function())
+
+    inner_function()
 
 
-print(test_function())
-print(inner_function())
+test_function()
+
