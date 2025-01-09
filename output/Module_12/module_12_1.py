1a1e4d9ed9dd171ff95076ecf460e5de765f12a8
TheTymanIRL <artiym@inbox.ru>
2025-01-09 19:28:55 +0500

    new

---
+++ output/Module_12/module_12_1.py
@@ -0,0 +1,36 @@
+import unittest
+
+class Runner:
+    def __init__(self, name):
+        self.name = name
+        self.distance = 0
+
+    def run(self):
+        self.distance += 10
+
+    def walk(self):
+        self.distance += 5
+
+    def __str__(self):
+        return self.name
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        runner = Runner('John')
+        for _ in range(10):
+            runner.walk()
+        self.assertEqual(runner.distance, 50)
+
+    def test_run(self):
+        runner = Runner('John')
+        for _ in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+
+    def test_challenge(self):
+        runner_1 = Runner('John')
+        runner_2 = Runner('Rik')
+        for _ in range(10):
+            runner_1.run()
+            runner_2.walk()
+        self.assertNotEqual(runner_1.distance, runner_2.distance)
